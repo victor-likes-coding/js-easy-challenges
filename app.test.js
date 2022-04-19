f579d71ed0fd8e3943117fb592d65c29cda3cfc4
Victor Tran <victor-likes-coding@tempestas.io>
2022-04-19 19:43:04 -0400

    write test

--- app.test.js
+++ app.test.js
@@ -8,3 +8,15 @@ describe("Sum function", () => {
         expect(sum(-1, -1)).toBe(-2);
     });
 });
+
+describe("Hour to second conversion function", () => {
+    test("2 hours => 7200 seconds", () => {
+        expect(hoursIntoSeconds(2)).toBe(7200);
+    });
+    test("10 hours => 36000 seconds", () => {
+        expect(hoursIntoSeconds(10)).toBe(36000);
+    });
+    test("24 hours => 86400 seconds", () => {
+        expect(hoursIntoSeconds(24)).toBe(86400);
+    });
+});
