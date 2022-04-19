52c1f4ba6a07e5060a212a1c3c6e7a93896e94d1
Victor Tran <victor-likes-coding@tempestas.io>
2022-04-19 19:45:14 -0400

    write function to convert hours into seconds

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const sum = (a, b) => {
     return a + b;
 };
 
+const hoursIntoSeconds = (hours) => {
+    const minutes = 60;
+    const seconds = 60;
+    return hours * minutes * seconds;
+};
+
 module.exports = {
     sum,
+    hoursIntoSeconds,
 };
